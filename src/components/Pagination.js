f696785d260a695cd0567beaebf8e707b005b2d3
Shubham <shubh.chavhan00@gmail.com>
2021-10-03 12:02:33 +0530

    added Pagination components for the posts

---
+++ src/components/Pagination.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "../App.css";
+
+export const Pagination = ({ postsPerPage, totalPosts, paginate }) => {
+  const pageNumbers = [];
+
+  for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
+    pageNumbers.push(i);
+  }
+
+  return (
+    <div>
+      <nav>
+        <ul>
+          {pageNumbers.map((number) => (
+            <li key={number}>
+              {/* <a onClick={() => paginate(number)} href="#!">
+                {number}
+              </a> */}
+              <button onClick={() => paginate(number)}> {number}</button>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </div>
+  );
+};
